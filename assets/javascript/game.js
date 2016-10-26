2b8c1dd232c4ab8da235f62309f92dc8da547a68
Jeff Swearingen <jeffswearingen@gmail.com>
2016-10-25 21:22:26 -0400

    move character to hero

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -36,14 +36,81 @@ function gameStart() {
 		img: "assets/images/Palpatine.jpg"
 	};
 
-	var characterDisplay = $("<div>");
-	characterDisplay.data('health', obiwan.health);
-	characterDisplay.attr('name', obiwan.name);
-	$('.characters').append();
+	// Obi-wan
+	var obiwanBlock = $('<div>');
+	$(obiwanBlock).attr('name', obiwan.name);
+	$(obiwanBlock).data('hp', obiwan.health);
+	$(obiwanBlock).addClass('characterToSelect');
+	$(obiwanBlock).text(obiwan.name);
+	$('.characters').append($(obiwanBlock));
+	var obiwanImage = $('<img>');
+	$(obiwanImage).attr('src', obiwan.img);
+	$(obiwanBlock).append($(obiwanImage));
+	var obiwanHp = $('<p>');
+	$(obiwanHp).text(obiwan.health);
+	$(obiwanBlock).append($(obiwanHp));
 
-	var name = $("<p>");
-	name.text(obiwan.name);
-	$(characterDisplay).append(name);
+	// Luke
+	var lukeBlock = $('<div>');
+	$(lukeBlock).attr('name', luke.name);
+	$(lukeBlock).data('hp', luke.health);
+	$(lukeBlock).addClass('characterToSelect');
+	$(lukeBlock).text(luke.name);
+	$('.characters').append($(lukeBlock));
+	var lukeImage = $('<img>');
+	$(lukeImage).attr('src', luke.img);
+	$(lukeBlock).append($(lukeImage));
+	var lukeHp = $('<p>');
+	$(lukeHp).text(luke.health);
+	$(lukeBlock).append($(lukeHp));
+
+	// Mace Windu
+	var winduBlock = $('<div>');
+	$(winduBlock).attr('name', windu.name);
+	$(winduBlock).data('hp', windu.health);
+	$(winduBlock).addClass('characterToSelect');
+	$(winduBlock).text(windu.name);
+	$('.characters').append($(winduBlock));
+	var winduImage = $('<img>');
+	$(winduImage).attr('src', windu.img);
+	$(winduBlock).append($(winduImage));
+	var winduHp = $('<p>');
+	$(winduHp).text(windu.health);
+	$(winduBlock).append($(winduHp));
+
+	// Darth Vader
+	var vaderBlock = $('<div>');
+	$(vaderBlock).attr('name', vader.name);
+	$(vaderBlock).data('hp', vader.health);
+	$(vaderBlock).addClass('characterToSelect');
+	$(vaderBlock).text(vader.name);
+	$('.characters').append($(vaderBlock));
+	var vaderImage = $('<img>');
+	$(vaderImage).attr('src', vader.img);
+	$(vaderBlock).append($(vaderImage));
+	var vaderHp = $('<p>');
+	$(vaderHp).text(vader.health);
+	$(vaderBlock).append($(vaderHp));
+
+	// Emperor Palpatine
+	var palpatineBlock = $('<div>');
+	$(palpatineBlock).attr('name', palpatine.name);
+	$(palpatineBlock).data('hp', palpatine.health);
+	$(palpatineBlock).addClass('characterToSelect');
+	$(palpatineBlock).text(palpatine.name);
+	$('.characters').append($(palpatineBlock));
+	var palpatineImage = $('<img>');
+	$(palpatineImage).attr('src', palpatine.img);
+	$(palpatineBlock).append($(palpatineImage));
+	var palpatineHp = $('<p>');
+	$(palpatineHp).text(palpatine.health);
+	$(palpatineBlock).append($(palpatineHp));
+
+	$('.characterToSelect').on('click', function() {
+		$(this).removeClass('.characterToSelect');
+		$(this).addClass('.hero');
+		$('.hero').append(this);
+	});
 };
 
 
